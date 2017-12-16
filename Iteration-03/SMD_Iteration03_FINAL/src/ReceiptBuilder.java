b4d719affc4c841477ee017596ccc57598b0c810
Thatcher Rickertsen <thatcherr2@gmail.com>
2017-12-16 13:39:38 -0600

    I messed up

--- Iteration-03/SMD_Iteration03_FINAL/src/ReceiptBuilder.java
+++
@@ -1,7 +0,0 @@
-public interface ReceiptBuilder {
-    public void setHeader(String storeName, int orderID, String cashier, String date);
-    public void addLine(String productID, String productName, String quantity, String price);
-    public void setFooter(String cost, String tax, String finalCost);
-    public String toString();
-    public void clearStringBuilder();
-}
